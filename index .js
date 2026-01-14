71206f55a5d20153b6c37e22e3252a1333dba555
Srijan0405 <srijanhardik16@gmail.com>
2026-01-14 20:36:45 +0530

    [Srijan] Remove screenshots from main branch

---
+++ index .js
@@ -0,0 +1,44 @@
+console.log("Node.js Backend Started");
+let name = "Aman";
+let age= 22;
+const country ="India";
+let isStudent = true;
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
+
+let score ;
+let data=null;
+console.log(score);
+console.log(data);
+
+let user = {
+    name :"Rahul",
+    age: 25,
+    email: "rahul@gmail.com",
+};
+
+console.log(user);
+console.log(user.name);
+
+let numbers=[10,20,30,40];
+let users=["Aman", "Ravi", "Neha"];
+
+console.log(numbers);
+console.log(users[1]);
+
+let a=10;
+let b=5;
+
+console.log("Add:", a+b);
+console.log("Sub:", a-b);
+console.log("Mul:", a*b);
+console.log("Div:", a/b);
+
+let loginAge = 18;
+if (loginAge=18){
+    console.log("Allowed to login");
+}else{
+    console.log("Not allowed");
+}
